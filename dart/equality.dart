14218ae04ecf00ae50ffc59d53d07fe456c99a4c
KLD <kld0093@hotmail.com>
2022-11-19 20:36:42 +0300

    seperated dart

---
+++ dart/equality.dart
@@ -0,0 +1,104 @@
+// void main() {
+//   int x = 1;
+//   int y = 1;
+
+//   if (x == y) {
+//     print("x equals y");
+//   } else {
+//     print("x doesn't equal y");
+//   }
+// }
+
+// void main() {
+//   String x = "one";
+//   String y = "one";
+
+//   if (x == y) {
+//     print("x equals y");
+//   } else {
+//     print("x doesn't equal y");
+//   }
+// }
+
+// void main() {
+//   List x = [];
+//   List y = [];
+
+//   if (x == y) {
+//     print("x equals y");
+//   } else {
+//     print("x doesn't equal y");
+//   }
+// }
+
+// void main() {
+//   List x = [];
+//   List y = x;
+
+//   if (x == y) {
+//     print("x equals y");
+//   } else {
+//     print("x doesn't equal y");
+//   }
+// }
+
+// void main() {
+//   Map x = {};
+//   Map y = {};
+
+//   if (x == y) {
+//     print("x equals y");
+//   } else {
+//     print("x doesn't equal y");
+//   }
+// }
+
+// void main() {
+//   Map x = {};
+//   Map y = x;
+
+//   if (x == y) {
+//     print("x equals y");
+//   } else {
+//     print("x doesn't equal y");
+//   }
+// }
+
+// void main() {
+//   Object x = Object();
+//   Object y = Object();
+
+//   if (x == y) {
+//     print("x equals y");
+//   } else {
+//     print("x doesn't equal y");
+//   }
+// }
+
+class Human {
+  final String name;
+
+  Human(this.name);
+}
+
+// void main() {
+//   Human x = Human("Khaled");
+//   Human y = Human("Khaled");
+
+//   if (x == y) {
+//     print("x equals y");
+//   } else {
+//     print("x doesn't equal y");
+//   }
+// }
+
+// void main() {
+//   Human x = Human("Khaled");
+//   Human y = x;
+
+//   if (x == y) {
+//     print("x equals y");
+//   } else {
+//     print("x doesn't equal y");
+//   }
+// }
